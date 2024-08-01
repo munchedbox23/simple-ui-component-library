5860abae4aa5cb7837393d6193607c00b2ad5102
Egor <egor032324@mail.ru>
2024-08-01 15:23:57 +0300

    docs: write documentation for the component library

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,16 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import path from "path";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../docs/**/*.mdx",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "storybook-addon-styled-component-theme/register",
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -17,19 +18,6 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
-  },
-  core: {
-    builder: "@storybook/builder-vite",
-  },
-  viteFinal: async (config) => {
-    config.plugins?.push(
-      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
-      tsconfigPaths({
-        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
-      })
-    );
-
-    return config;
   },
 };
 export default config;
